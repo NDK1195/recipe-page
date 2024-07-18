e5cb330405d90b08d47fd70f90c09272bd40a3b2
ndkhoa1195 <ndkhoa1195@gmail.com>
2024-07-18 14:13:27 +0700

    add project structure

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    extend: {
+      fontFamily: {
+        'young-serif': ['Young Serif', 'serif'],
+        outfit: ['Outfit', 'sans-serif'],
+      },
+      colors: {
+        'rose-800': '#7A284E',
+        'rose-50': '#FFF7FB',
+        'stone-900': '#312E2C',
+        'stone-600': '#5F564D',
+        'stone-150': '#E3DDD7',
+        'stone-100': '#F3E5D7',
+        'brown-800': '#854632',
+      },
+    },
+  },
+  plugins: [],
+};
